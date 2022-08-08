0eee72173b8e7b9c73ad3dd3d553b304be4ca026
vanf-x <facundoaguirre1989@gmail.com>
2022-08-08 18:55:44 -0300

    modificación del footer para que no se rompa, subida a vercel

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
-import "./Footer.css"
-import React from 'react'
+import "./Footer2.css";
+import React from "react";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -12,62 +12,93 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="containerFooter">
-      <div className="cgi">
-        <h1 style={{margin:'2%'}}>CGI CONSULTING INC.</h1>
-        <img src='/img/Logo-removebg-previewEdited.png' alt="Logo CGI." className="logo" />
-        <p>Consultoría y Gestión para la Inversión en Infraestructura</p>
-      </div>
+      {/* //contacto */}
+      <div className="containerFooter-up ">
+        <div className="cgi box center">
+          <h1 style={{ margin: "2%" }}>CGI CONSULTING INC.</h1>
+          <img
+            src="/img/Logo-removebg-previewEdited.png"
+            alt="Logo CGI."
+            className="logo"
+          />
+          <p>Consultoría y Gestión para la Inversión en Infraestructura</p>
+        </div>
+        {/* //redes */}
 
-      <div className="contactUs">
-        <h2 style={{margin:'2%'}}>Contáctenos</h2>
-        <div className="email">
+        <div className="contactUs box center">
+          <h2 style={{ margin: "2%" }}>Contáctenos</h2>
+          <div className="email">
             <FontAwesomeIcon
               icon={faEnvelope}
               className="nav-icon"
             ></FontAwesomeIcon>
             <p>info@cgiconsulting.co</p>
+          </div>
+          <div className="iconsRedes">
+            <a
+              href="https://www.facebook.com/cgiconsultinginc"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faFacebookF}
+                className="nav-icon"
+              ></FontAwesomeIcon>
+            </a>
+            <a
+              href="https://www.linkedin.com/company/cgiconsulting/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                className="nav-icon"
+              ></FontAwesomeIcon>
+            </a>
+            <a
+              href="https://www.instagram.com/cgiconsulting/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="nav-icon"
+              ></FontAwesomeIcon>
+            </a>
+          </div>
         </div>
-        <div className="iconsRedes">
-          <a href="https://www.facebook.com/cgiconsultinginc" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon
-              icon={faFacebookF}
-              className="nav-icon"
-            ></FontAwesomeIcon>
-          </a>
-          <a
-            href="https://www.linkedin.com/company/cgiconsulting/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faLinkedinIn}
-              className="nav-icon"
-            ></FontAwesomeIcon>
-          </a>
-          <a href="https://www.instagram.com/cgiconsulting/" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon
-              icon={faInstagram}
-              className="nav-icon"
-            ></FontAwesomeIcon>
-          </a>
+        {/* //servicios */}
+        <div className="services_footer box">
+            <h2 style={{ margin: "2%" }}>Servicios</h2>
+        <ul>
+          <li>
+            <Link to="/servicios">Ingeniería y Construcción</Link>
+          </li>
+          <li>
+            <Link to="/servicios">Energía y Automatización</Link>
+          </li>
+          <li>
+            <Link to="/servicios">Bienes Raíces Industriales</Link>
+          </li>
+          <li>
+            <Link to="/servicios">Suministro y Automatización</Link>
+          </li>
+          <li>
+            <Link to="/servicios">Gestión de Riesgos y Seguros</Link>
+          </li>
+        </ul>
+      </div>
+      </div>
+
+      {/* <div className="line"></div> */}
+      <div className="containerFooter-down">
+        <div className="copyright">
+          Copyright 2022.Todos los derechos reservados.
         </div>
       </div>
-      <Link to="/servicios">
-          <div className="services_footer">
-          <h2 style={{margin:'2%'}}>Servicios</h2>
-          <p>Ingeniería y Construcción</p>
-          <p>Energía y Automatización</p>
-          <p>Bienes Raíces Industriales</p>
-          <p>Suministro y Automatización</p>
-          <p>Gestión de Riesgos y Seguros</p>
-        </div>
-      </Link>
 
-      <div className="line"></div>
-      
-      <div className="copyright">Copyright  2022.Todos los derechos reservados.</div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,6 @@ import React from "react";
 const Header = (props) => {
     return <>
             <div className="containerTitle">
-                <img src="/img/fondo-titulo.png" alt="fondo" className="imgBackgroundTitle"/>
                 <h1 className="titleSeccion">{props.titulo}</h1>
             </div>
         </>;
